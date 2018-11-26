338ec6a04eee6196a8d9a65acba27e8b8cc515eb
Christian Svensson <blue@cmd.nu>
2018-11-27 00:48:14 +0100

    wip

--- cmd/leopatch/main.go
+++ cmd/leopatch/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/dhtech/leopatch/ast"
@@ -36,9 +37,84 @@ func main() {
 	fmt.Printf(" [>] Chip ID: %06x\n", f.Id())
 
 	loc, size := scan(f)
-	dump(f, loc, size, "root.bin")
+	if _, err := os.Stat("/tmp/root.bin"); os.IsNotExist(err) {
+		// 0x40 = the uImage header
+		dump(f, loc + 0x40, size - 0x40, "/tmp/root.bin")
+		fmt.Printf(" [*] Root dumped\n")
+	} else {
+		fmt.Printf(" [*] Previous root dump found, using it\n")
+	}
 
-	fmt.Printf(" [*] Root dumped\n")
+	_ = os.RemoveAll("/tmp/bmc-root")
+
+	fmt.Printf(" [*] Extracting root\n")
+	c := exec.Command("/usr/sbin/cramfsck", "-x", "/tmp/bmc-root", "/tmp/root.bin")
+	if err := c.Run(); err != nil {
+		log.Fatalf("Failed to extract root: %v", err)
+	}
+
+	fmt.Printf(" [*] Modifying root\n")
+	if err := os.Symlink("/bin/ash", "/tmp/bmc-root/usr/local/bin/smash"); err != nil {
+		log.Fatalf("Failed to create shell symlink: %v", err)
+	}
+
+	fmt.Printf(" [*] Compressing root\n")
+	c = exec.Command("/usr/sbin/mkcramfs", "/tmp/bmc-root", "/tmp/root-new.bin")
+	if err := c.Run(); err != nil {
+		log.Fatalf("Failed to create new root: %v", err)
+	}
+
+	fmt.Printf(" [*] Creating uImage\n")
+	c = exec.Command(
+		"/usr/bin/mkimage", "-A", "arm", "-O", "linux", "-T", "ramdisk",
+		"-C", "none", "-d", "/tmp/root-new.bin", "/tmp/root-image.bin")
+	if err := c.Run(); err != nil {
+		log.Fatalf("Failed to create new root: %v", err)
+	}
+
+	fmt.Printf(" [*] Writing hacked uImage\n")
+	write(f, loc, "/tmp/root-image.bin")
+
+	fmt.Printf(" [*] All done, have a nice day\n")
+}
+
+func write(f ast.Flash, loc int64, fp string) {
+	buf := make([]byte, 64*1024)
+
+	df, err := os.Open(fp)
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer df.Close()
+	size, err := df.Seek(0, 2)
+	if err != nil {
+		log.Fatalf("Failed to seek to end of data file: %v", err)
+	}
+  _, err = df.Seek(0, 0)
+	if err != nil {
+		log.Fatalf("Failed to seek to start of data file: %v", err)
+	}
+	all := size
+	fmt.Printf(" [*] Write progress: 0%%")
+	for {
+		n, err := df.Read(buf)
+		if err != nil {
+			log.Fatalf("Failed to read: %v", err)
+		}
+		fmt.Printf("\r [*] Write progress: %d%%", 100 - (size * 100 / all))
+		loc += int64(len(buf))
+		if size <= int64(n) {
+			_, err = f.WriteAt(buf[:size], loc)
+			break
+		} else {
+			_, err = f.WriteAt(buf[:n], loc)
+		}
+		if err != nil {
+			log.Fatalf("Failed to write: %v", err)
+		}
+		size -= int64(n)
+	}
+	fmt.Printf("\r [*] Write complete\n")
 }
 
 func dump(f ast.Flash, loc int64, size int, fp string) {
@@ -58,7 +134,7 @@ func dump(f ast.Flash, loc int64, size int, fp string) {
 		}
 		fmt.Printf("\r [*] Read progress: %d%%", 100 - (size * 100 / all))
 		loc += int64(len(buf))
-		if size < n {
+		if size <= n {
 			df.Write(buf[:size])
 			break
 		} else {
@@ -92,9 +168,9 @@ func scan(f ast.Flash) (int64, int) {
 				fmt.Printf(" [>>] Partition %v @ %x \n", string(name), offset)
 				if string(name[:4]) == "root" {
 					fmt.Printf(" [*] Found the root, size is %d MiB\n", ssize / 1024 / 1024)
-					// TODO(bluecmd): 0x40 seems to be the FMH size, and then 1 erase block
-					// seems to be the offset. If this breaks, look at scanning for cramfs
-					return offset + 0x10040, ssize - 0x10040
+					// TODO(bluecmd): Offset seems to be 1 erase block.
+					// If this breaks, look at scanning for cramfs / uImage.
+					return offset + 0x10000, ssize - 0x10000
 				}
 				offset = (offset + int64(ssize)) & 0xffff0000
 				continue
